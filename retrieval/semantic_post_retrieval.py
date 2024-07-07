8342648e20bf76b0791cce2bd05ff1ae501451ed
Fede-stack <75915084+Fede-stack@users.noreply.github.com>
2024-07-07 18:40:29 +0200

    Update semantic_post_retrieval.py

--- retrieval/semantic_post_retrieval.py
+++ retrieval/semantic_post_retrieval.py
@@ -11,9 +11,9 @@ tokenizer = AutoTokenizer.from_pretrained(model_choice)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 
-X_ = []  # lista di documenti recuperati
-y_ = []  # lista di punteggi associati a ciascun documento
-error_indices = []  # lista per salvare gli indici con errore
+X_ = []  #list of retrieved documents
+y_ = []  #list of scores associated to each retrieved individual's item
+error_indices = []  
 
 for i, bdi in enumerate(bdi_items):
     print(i)
@@ -29,11 +29,11 @@ for i, bdi in enumerate(bdi_items):
         with torch.no_grad():
             doc_embeddings = model(**inputs_doc, output_hidden_states=True, return_dict=True).hidden_states[-1][:, :1].squeeze(1)
         doc_embeddings = doc_embeddings.cpu().numpy()
-        # embeddings per documenti
+        #embeddings per documenti
 #         doc_embeddings = model.encode(docs)
 
         documents_retrieved = []
-        # per ciascuna query, trova i post più simili
+        #for each query, find similar posts
         for query_emb in query_embeddings:
             try:
                 embs = np.concatenate((query_emb.reshape(1, -1), doc_embeddings))
